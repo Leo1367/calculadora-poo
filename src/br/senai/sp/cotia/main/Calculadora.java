43bb07f389aaaa4d5ad2e3e05ddcc00c4b7c22df
Edilando <35576130+EdilandoSaturnino@users.noreply.github.com>
2023-05-07 12:33:59 -0300

    att

--- src/br/senai/sp/cotia/main/Calculadora.java
+++ src/br/senai/sp/cotia/main/Calculadora.java
@@ -6,9 +6,6 @@ import java.util.Scanner;
 public class Calculadora {
 
 	public static void main(String[] args) {
-		
-		System.out.println("banana");
-		System.out.println("maça"); 
 
 		Scanner leitor = new Scanner(System.in);
 		ArrayList<String> historico = new ArrayList<String>();
