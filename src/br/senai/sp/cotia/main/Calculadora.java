bb985edb3ca9aed7f05320d5c5833af06f19f49e
Edilando <35576130+EdilandoSaturnino@users.noreply.github.com>
2023-05-07 12:05:58 -0300

    adicionei a banana

--- src/br/senai/sp/cotia/main/Calculadora.java
+++ src/br/senai/sp/cotia/main/Calculadora.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Calculadora {
 
 	public static void main(String[] args) {
+		
+		System.out.println("banana");
 
 		Scanner leitor = new Scanner(System.in);
 		ArrayList<String> historico = new ArrayList<String>();
